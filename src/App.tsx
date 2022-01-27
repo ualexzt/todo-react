92840afc0f30d1fed2a7f94bb9b13a9e3aa28ba0
AlexUa <ualexzt@gmail.com>
2022-01-27 17:32:09 +0200

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
+import { TextField } from '@mui/material';
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TextField label="Add todo" variant="outlined" />
+    </>
   );
 }
 
